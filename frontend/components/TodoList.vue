6eecb9fdef735047b98c779785475570a92d2e87
竹下裕人 <pedkd.hiro.vet@gmail.com>
2022-08-16 17:50:05 +0900

    add TodoListComponent

---
+++ frontend/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          title: "test",
+          username: "太郎"
+        }
+      ],
+      search: "",
+      headers: [
+        {
+          text: "タイトル", //先頭のヘッダーの内容
+          align: "left", //テーブルに対して文字を左詰め
+          sortable: false,
+          value: "title" //itemsのプロパティに含まれる値を表示
+        },
+        { text: "ユーザー名", 
+          value: "username"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
