add7c031606dbb95ccbd220a96ec72e6001c7369
竹下裕人 <pedkd.hiro.vet@gmail.com>
2022-08-17 14:48:05 +0900

    フロントサイドとバックエンドの連携

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -8,6 +8,7 @@
 <script>
   import AddTodo from "@/components/AddTodo";
   import TodoList from "@/components/TodoList";
+  import axios from "@/plugins/axios"; 
 
   export default {
     components: {
@@ -20,7 +21,9 @@
       };
     },
     methods: {
-      addTodo(title) {
+     async addTodo(title) {
+      //サーバーサイドへリクエストをかける前に実行する処理
+      await axios.post("/api/v1/todos", { title }); 
         this.todos.push({
           title
         });
