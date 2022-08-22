4034abcfb99eae6893f83a88e0e9616712d2e6cf
竹下裕人 <pedkd.hiro.vet@gmail.com>
2022-08-22 23:29:56 +0900

    ナビゲーションガードの実装

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -39,7 +39,20 @@
        });
       },
     },
-  };
+    fetch({
+      store,
+      redirect
+    }) {
+      store.watch(
+        state => state.auth.currentUser,
+        (newUser, oldUser) => {
+          if (!newUser) {
+            return redirect("/login");
+          }
+        }
+    );
+  },
+};
 
 </script>
 
